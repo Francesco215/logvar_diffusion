9fb552ed14d932676e351fe8b0450dae1a99bec6
Francesco Sacco <47751420+Francesco215@users.noreply.github.com>
2025-08-05 01:07:51 +0200

    Update training.py

--- training.py
+++ training.py
@@ -1,10 +1,3 @@
-# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-#
-# This work is licensed under a Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-# You should have received a copy of the license along with this
-# work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
-
 """
 Dependency-free version of the original 2D toy example from the paper
 "Guiding a Diffusion Model with a Bad Version of Itself".
